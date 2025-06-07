6cbf0683c150cc8f00d7c2c1b07b780ef033043a
Daksh <crushgaming990@gmail.com>
2025-06-07 11:07:46 +0530

    deployed the project

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,4 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api" // your local backend during dev
-    : "https://realtime-spotify-clone-w2xn.onrender.com/api", // your live backend
-});
+  baseURL: "https://realtime-spotify-clone-w2xn.onrender.com/api",});
